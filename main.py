fa0b1bb253b867297866f7c53a811eeb32a9c9a6
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-23 20:33:37 +0100

    func bonjour

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import os
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
+
+@bot.command()
+async def bonjour (context):
+    await context.send(f"Bonjour ! {context.author}")
