5fb81dae0349f4fab890750919d4b4d68ca7d02c
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-23 23:37:23 +0100

    ajout /history qui affiche uniquement les commandesentrés pour le moment

--- main.py
+++ main.py
@@ -2,14 +2,60 @@ import os
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
+TOKEN = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+print ('Lancement du bot ...')
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@bot.command()
-async def bonjour (context):
-    await context.send(f"Bonjour ! {context.author}")
+@bot.event
+async def on_ready(): 
+    print("Bot allumé ")
+    #Synchroniser commandes
+    try:
+        synchronise = await bot.tree.sync()
+        print(f"Commandes '/' synchronisées: {len(synchronise)}")
+    except Exception as e:
+        print(e)
 
-TOKEN = os.environ['TOKEN_DISCORD']
+@bot.event
+async def on_message(message: discord.Message):
+    #Pour pas que le bot se declenche lui meme
+    if message.author.bot:
+        return
+    
+    if message.content.lower() =='quoi':
+        channel = message.channel
+        await channel.send("FEUR !")
+
+    if message.content.lower() == 'ntm':
+        channel = message.channel
+        author = message.author
+        await author.send("Tu veux je bz ta mère ?")
+
+    if message.content.lower() == 'au revoir':
+        good_bye_channel = bot.get_channel(1442268548436332765)
+        await good_bye_channel.send(f"Au revoir {message.author} !")
+
+@bot.tree.command(name="history", description="Affiche l'historique des commandes")
+async def history(interaction: discord.Interaction):
+    user = interaction.user
+    channel = interaction.channel
+    #eviter les erreurs si trop long
+    await interaction.response.defer()
+
+    commandes = []
+    async for msg in channel.history(limit=100):
+        if msg.author.id == user.id:
+            commandes.append(f" - {msg.content}")
+    if not commandes:
+        await interaction.followup.send("Pas de commandes dans l'historique")
+        return
+    
+    history_commandes = "\n".join(commandes)
+
+    if len(history_commandes) > 2000:
+        history_commandes = history_commandes[:1995] + "\n..."
+    await interaction.followup.send(f"Historique des tes commandes:\n{history_commandes}")
+
+
 bot.run(TOKEN)
