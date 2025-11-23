2d5493fa88f719f99b23c88c06cd587c556f882f
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-23 23:14:31 +0100

    ajout commande /

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import os
 import discord
+from discord.ext import commands
 
 TOKEN = os.getenv("TOKEN")
 
 print ('Lancement du bot ...')
-bot = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready(): 
     print("Bot allumé ")
+    #Synchroniser commandes
+    try:
+        synchronise = await bot.tree.sync()
+        print(f"Commandes '/' synchronisées: {len(synchronise)}")
+    except Exception as e:
+        print(e)
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -28,5 +35,9 @@ async def on_message(message: discord.Message):
     if message.content.lower() == 'au revoir':
         good_bye_channel = bot.get_channel(1442268548436332765)
         await good_bye_channel.send(f"Au revoir {message.author} !")
+
+@bot.tree.command(name="youtube")
+async def youtube(interaction: discord.Interaction):
+    await interaction.response.send_message("Voici le lien de la vidéo : https://youtu.be/xvFZjo5PgG0?si=kTfrnDxnsbj0axPe")
 
 bot.run(TOKEN)
