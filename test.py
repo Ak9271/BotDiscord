39b6a941d06d4da1ff0a68e6c7eeae9c5d267732
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-23 22:55:15 +0100

    repond dans un autre channel

--- test.py
+++ test.py
@@ -12,12 +12,21 @@ async def on_ready():
 
 @bot.event
 async def on_message(message: discord.Message):
+    #Pour pas que le bot se declenche lui meme
+    if message.author.bot:
+        return
+    
     if message.content.lower() =='quoi':
         channel = message.channel
         await channel.send("FEUR !")
+
     if message.content.lower() == 'ntm':
         channel = message.channel
         author = message.author
         await author.send("Tu veux je bz ta mère ?")
+        
+    if message.content.lower() == 'au revoir':
+        good_bye_channel = bot.get_channel(1442268548436332765)
+        await good_bye_channel.send("Au revoir.")
 
 bot.run(TOKEN)
