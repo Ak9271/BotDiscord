6d154767815941d29d680bbaf727b76d62599568
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-23 22:16:51 +0100

    repond salut a bonjour

--- test.py
+++ test.py
@@ -2,7 +2,18 @@ import os
 import discord
 
 TOKEN = os.getenv("TOKEN")
-bot = discord.Client(intents=discord.Intents.all())
-bot.run(TOKEN)
 
-print ('Lancement du bot')
+print ('Lancement du bot ...')
+bot = discord.Client(intents=discord.Intents.all())
+
+@bot.event
+async def on_ready(): 
+    print("Bot allumé ")
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.content =='bonjour':
+        channel = message.channel
+        await channel.send("Salut")
+
+bot.run(TOKEN)
