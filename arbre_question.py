6c377c4f1f3c85326be1690e63fd2fcad6b2e1df
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-27 22:35:52 +0100

    generer question  2 et 3

--- arbre_question.py
+++ arbre_question.py
@@ -9,11 +9,20 @@ def generer_calcul_1():
     reponse = a + b * c
     return question, reponse
 
+def generer_calcul_2():
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    c = random.randint(1, 20)
+    question = f"2. Calculer : ({a} - {b}) * {c}"
+    reponse = (a - b) * c
+    return question, reponse
 
-
-
-
-
+def generer_calcul_3():
+    a = random.randint(1, 1000)
+    b = random.randint(1, 9)
+    question = f"3. Calculer : {a} / {b} (arrondir à l'entier le plus proche)"
+    reponse = round(a / b)
+    return question, reponse
 
 ArbreQuestion = {
 
