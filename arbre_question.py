89e220140da0c8b9bd10c849c1411efa777f9904
nakchaya <akchayan.nihalananth@ynov.com>
2025-11-27 22:44:45 +0100

    generer question 4 et 5

--- arbre_question.py
+++ arbre_question.py
@@ -19,10 +19,25 @@ def generer_calcul_2():
 
 def generer_calcul_3():
     a = random.randint(1, 1000)
-    b = random.randint(1, 9)
+    b = random.randint(1, 10)
     question = f"3. Calculer : {a} / {b} (arrondir à l'entier le plus proche)"
     reponse = round(a / b)
     return question, reponse
+
+def generer_calcul_4():
+    pourcentage = random.choice([10,15,20,25,30])
+    nombre = random.choice([50,100,150,200,250,500])
+    question = f"4. Calculer : {pourcentage}% de {nombre}"
+    reponse = (pourcentage / 100) * nombre
+    return question, reponse
+
+def generer_calcul_5():
+    nbr_virgule = random.choice([0.1,0.2,0.5,1.5,2.5,3.5,4.5,5.5])
+    nombre = random.randint(1, 1000)
+    question = f"5. Calculer : {nombre} * {nbr_virgule}"
+    reponse = nombre * nbr_virgule
+    return question, reponse
+
 
 ArbreQuestion = {
 
